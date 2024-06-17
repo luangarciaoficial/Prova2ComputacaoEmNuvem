b8c4dc7666f0640a6bdb8576ee899980bd2c0b5a
Luan Garcia <luangarciaoficial@gmail.com>
2024-06-17 14:42:06 -0300

    first commit

---
+++ app/views/classes/home.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<h1>Bem-vindo à nossa loja de Ferros!</h1>";
+    echo "<p>Aqui sempre estamos Ferrados</p>";
+?>
---
+++ app/views/classes/produtos.php
@@ -0,0 +1,49 @@
+<?php
+    echo "<h2>Nossos Produtos</h2>";
+
+    echo "<div class='mainproducts'>";
+    
+    echo "<div class='produto'>";
+    echo "<h3>Barra de ferro</h3>";
+    echo "<img class='imgproduct' src='../img/barraferro.png'>";
+    echo "<p>R$ 49,90</p>";
+    echo "<button class='add-carrinho' data-produto='Barra de ferro'>Adicionar ao Carrinho</button>";
+    echo "</div>";
+
+    echo "<div class='produto'>";
+    echo "<h3>Tubo de ferro</h3>";
+    echo "<img class='imgproduct' src='../img/tuboferro.png'>";
+    echo "<p>R$ 79,90</p>";
+    echo "<button class='add-carrinho' data-produto='Tubo de ferro'>Adicionar ao Carrinho</button>";
+    echo "</div>";
+
+    echo "<div class='produto'>";
+    echo "<h3>Barra de ferro</h3>";
+    echo "<img class='imgproduct' src='../img/barraferro.png'>";
+    echo "<p>R$ 49,90</p>";
+    echo "<button class='add-carrinho' data-produto='Barra de ferro'>Adicionar ao Carrinho</button>";
+    echo "</div>";
+
+    echo "<div class='produto'>";
+    echo "<h3>Tubo de ferro</h3>";
+    echo "<img class='imgproduct' src='../img/tuboferro.png'>";
+    echo "<p>R$ 79,90</p>";
+    echo "<button class='add-carrinho' data-produto='Tubo de ferro'>Adicionar ao Carrinho</button>";
+    echo "</div>";
+
+    echo "<div class='produto'>";
+    echo "<h3>Barra de ferro</h3>";
+    echo "<img class='imgproduct' src='../img/barraferro.png'>";
+    echo "<p>R$ 49,90</p>";
+    echo "<button class='add-carrinho' data-produto='Barra de ferro'>Adicionar ao Carrinho</button>";
+    echo "</div>";
+
+    echo "<div class='produto'>";
+    echo "<h3>Tubo de ferro</h3>";
+    echo "<img class='imgproduct' src='../img/tuboferro.png'>";
+    echo "<p>R$ 79,90</p>";
+    echo "<button class='add-carrinho' data-produto='Tubo de ferro'>Adicionar ao Carrinho</button>";
+    echo "</div>";
+
+    echo "</div>";
+?>
